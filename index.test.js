a7d4f6d6782a1c3303ab76233ec0533d02315d83
Greg <remph819@gmail.com>
2020-03-22 19:39:29 -0700

    added more tests

--- index.test.js
+++ index.test.js
@@ -1,47 +1,32 @@
-const supertest = require('supertest')
-const server = require('./api/server')
-const db = require('./database/dbConfig')
+const supertest = require('supertest');
+const server = require('./api/server');
+const db = require('./database/dbConfig');
 
 beforeEach(async () => {
-    await db.seed.run()
-})
+	await db.seed.run();
+});
 
 afterAll(async () => {
-    await db.destroy()
-  })
-
-
+	await db.destroy();
+});
 
 test('see if user is registered', async () => {
-    const res = await supertest(server)
-    .post('/api/auth/register').send({username: "Manboy", password: "hello"})
-    expect(res.statusCode).toBe(201)
-    expect(res.type).toBe('application/json')
-    expect(res.body.name).toBe("Manboy")
-})
+	const res = await supertest(server).post('/api/auth/register').send({ username: 'Manboy', password: 'hello' });
+	expect(res.statusCode).toBe(201);
+	expect(res.type).toBe('application/json');
+});
 
-// test('see if username is taken' , async () => {
-//     const res = await supertest(server)
-//     .post('auth/register')
-//     .send({username: "Terminator", password: "2029"})
-//     expect(res.type).toBe('application/json')
-//     expect(res.statusCode).toBe(409)
-// })
+test('see if username is taken', async () => {
+	const res = await supertest(server).post('/api/auth/register').send({ username: 'Terminator', password: '2029' });
+	expect(res.statusCode).toBe(409);
+});
 
+test('see if login is successful', async () => {
+	const res = await supertest(server).post('/api/auth/login').send({ username: 'Terminator', password: '2029' });
+	expect(res.statusCode).toBe(200);
+});
 
-
-// test('see if login is successful', async () => {
-//     const res = await supertest(server)
-//     .post('/api/auth/login')
-//     .send({username: 'Terminator', password: "2029"})
-//     expect(res.status).toBe(200)
-// })
-
-
-// test('see if login failed', async () => {
-//     const res = await supertest(server)
-//     .post('/auth/login')
-//     .send({username: "Terminator", password: 2020})
-//     expect(res.status).toBe(401)
-//     expect(res.type).toBe('application/json')
-// })
+test('see if login failed', async () => {
+	const res = await supertest(server).post('/api/auth/login').send({ username: 'test', password: quinoa });
+	expect(res.statusCode).toBe(401);
+});
